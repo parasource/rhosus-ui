7496c7a412182372e2dc2e9135efd47497f562af
TiTUS <us.daniel_roberts@mail.ru>
2023-06-08 15:33:37 +0400

    add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,72 @@
+import styled from 'styled-components';
+import { logo } from '@/images';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+	return (
+		<HeaderWrapper>
+			<Link to='/'>
+				<Logo>
+					<img src={logo} alt='logo' />
+				</Logo>
+			</Link>
+
+			<Navigation>
+				<NavigationItem>
+					<Link to='/files'>
+						Files
+					</Link>
+				</NavigationItem>
+
+				<NavigationItem>
+					<Link to='/clusters'>
+						Clusters
+					</Link>
+				</NavigationItem>
+
+				<NavigationItem>
+					<Link to='/policies'>
+						Policies
+					</Link>
+				</NavigationItem>
+			</Navigation>
+		</HeaderWrapper>
+	);
+};
+
+const HeaderWrapper = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  background: #000;
+  padding: 30px 16px;
+`;
+
+const Navigation = styled.nav``;
+
+const NavigationItem = styled.span`
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 29px;
+  color: #FFFFFF;
+  transition: 0.2s;
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &:not(:last-child) {
+    margin-right: 32px;
+  }
+`;
+
+const Logo = styled.div`
+  margin-right: 64px;
+  transition: 0.2s;
+
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
+export default Header;
