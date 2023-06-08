b7656da98100b801f0abe8938d6cf7b66a0a5810
TiTUS <us.daniel_roberts@mail.ru>
2023-06-08 15:39:47 +0400

    add TitleContent component and setup Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
-import React from 'react';
-
 import { Provider } from 'react-redux';
 import { store } from '@/store/store';
-import { Files } from './components/Files';
+import Files from './modules/files';
+import Header from '@/components/Header';
+import styled from 'styled-components';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import TitleContent from '@/components/TitleContent';
+
 
 function App() {
+	const location = useLocation();
 
 	return (
-		<Provider store={store}>
-			<Files/>
-		</Provider>
+		<AppWrapper>
+			<Header />
+			<TitleContent title={location.pathname} />
+			<AppContent>
+				<Provider store={store}>
+					<Routes>
+						<Route path='/files' element={<Files />} />
+					</Routes>
+				</Provider>
+			</AppContent>
+		</AppWrapper>
 	);
 }
+
+const AppWrapper = styled.div``;
+
+const AppContent = styled.main`
+  max-width: 1440px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  padding-right: 20px;
+  padding-left: 20px;
+`;
 
 export default App;
